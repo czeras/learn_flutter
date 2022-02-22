6fc2e005754d0b869c6a249c1953a616b57220ce
chaomeng <custom_mail@163.com>
2022-02-22 18:10:50 +0800

    启动页配置

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## flutter_live 配置
 macOS：11.6.1
 Xcode：13.2.1
-Flutter (Channel stable, 2.2.0)
+Flutter (Channel stable, 2.8.1)
 
 仿某直播app
 
---
+++ flutter_live/lib/pages/launch_page.dart
@@ -0,0 +1,85 @@
+
+import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
+
+///启动页
+class LaunchPage extends StatefulWidget {
+  @override
+  _LaunchPageState createState() => _LaunchPageState();
+}
+
+class _LaunchPageState extends State<LaunchPage> {
+
+  String version = "";
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+      // if(CBStringUtil.isEmpty(Global.token)||UserManager.getInstance().doctorModel==null){
+      //   NavigatorUtil.goToLogin();
+      // }else{
+      //   if(UserManager.getInstance().doctorModel.positionNameList==null){
+      //     NavigatorUtil.goToPerfect();
+      //   }else{
+      //     NavigatorUtil.goToHome();
+      //   }
+      //
+      // }
+    });
+    getVersion();
+  }
+
+  void getVersion()async{
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      version = packageInfo.version;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      color: Colors.white,
+    );
+
+    // CBScreenFit.init(context);
+    // return Scaffold(
+    //   body: Stack(
+    //     children: [
+    //       LoadAssetImage("lanuch",width: Get.width,fit: BoxFit.cover,),
+    //       Positioned(
+    //           bottom: 60.rpx,
+    //           child: Container(
+    //             width: Get.width,
+    //             child: Row(
+    //               mainAxisAlignment: MainAxisAlignment.center,
+    //               children: [
+    //                 LoadAssetImage("lanuch_logo",width: 50.rpx,height: 50.rpx),
+    //                 AppGaps.hGap20,
+    //                 CBView(
+    //                   text:"春播助手",
+    //                   style: CBTextStyles.textBlack3_font22(isBold: true),
+    //                 )
+    //               ],
+    //             ),
+    //           )
+    //       ),
+    //       Positioned(
+    //           bottom: 20.rpx,
+    //           child: CBView(
+    //             width: Get.width,
+    //             textAlignment: Alignment.center,
+    //             text: "版本：v${version}",
+    //             style: CBTextStyles.textBlack9_font16(),
+    //           )
+    //       )
+    //     ],
+    //   ),
+    // );
+  }
+}
