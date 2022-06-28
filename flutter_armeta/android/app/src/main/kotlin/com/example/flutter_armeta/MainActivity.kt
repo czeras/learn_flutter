a0a5544f0e0248be9835455120df1bce5795c80d
chaomeng <custom_mail@163.com>
2022-06-29 00:24:33 +0800

    添加新文件

---
+++ flutter_armeta/android/app/src/main/kotlin/com/example/flutter_armeta/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_armeta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_demo/lib/screen_fit.dart
@@ -0,0 +1,109 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class CBScreenFit{
+  static late CBScreenFit _instance;
+  //默认UI效果图宽高
+  static const double defaultWidth = 375;
+  static const double defaultHeight = 812;
+
+  //实际UI效果图宽高
+  late double uiWidth;
+  late double uiHeight;
+
+  //实际屏幕
+  static late double _screenWidth;
+  static late double _screenHeight;
+
+  //设备像素密度
+  static late double _pixelRatio;
+
+  //状态栏高度
+  static late double _statusHeight;
+
+  //tabbar高度
+  static late double _bottomBarHeight;
+
+  //系统字体缩放比例
+  static late double _textScaleFactor;
+
+  static late MediaQueryData _mediaQueryData;
+
+
+  CBScreenFit._();
+
+  factory CBScreenFit(){
+    return _instance;
+  }
+
+  /*
+   * 在应用入口初始化
+   */
+  static void init(BuildContext context,{double width = defaultWidth,double height = defaultHeight}){
+    //单例
+    if(_instance==null){
+      _instance = CBScreenFit._();
+    }
+
+    //获取实际UI效果宽高
+    _instance.uiHeight = height;
+    _instance.uiWidth = width;
+
+    //获取当前设备屏幕信息
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    _mediaQueryData = mediaQuery;
+    _screenWidth = mediaQuery.size.width;
+    _screenHeight = mediaQuery.size.height;
+    _textScaleFactor = mediaQuery.textScaleFactor;
+    _statusHeight = mediaQuery.padding.top;
+    _bottomBarHeight = mediaQuery.padding.bottom;
+    _pixelRatio = mediaQuery.devicePixelRatio;
+
+  }
+
+  ///当前设备密度
+  static double get pixelRatio => _pixelRatio;
+
+  /// 当前设备宽度
+  static double get screenWidthPT => _screenWidth;
+
+  /// 当前设备高度
+  static double get screenHeightPT => _screenHeight;
+
+  /// 当前设备宽度
+  static double get screenWidthPX => _screenWidth*_pixelRatio;
+
+  /// 当前设备高度
+  static double get screenHeightPX => _screenHeight*_pixelRatio;
+
+  /// 状态栏高度 pt 刘海屏会更高
+  static double get statusBarHeight => _statusHeight;
+
+  /// 底部安全区距离 pt
+  static double get bottomBarHeight => _bottomBarHeight;
+
+  /// 宽度缩放比例
+  double get scaleWidth => _screenWidth / uiWidth;
+
+  /// 高度缩放比例
+  double get scaleHeight => _screenHeight / uiHeight;
+
+  /// 文字缩放
+  double get scaleText => scaleWidth;
+
+  ///适配后的屏幕宽度
+  num setWidth(num width){
+    if(_pixelRatio>=2){
+      return width * scaleWidth;
+    }else{
+      return width * scaleWidth/1.5;
+    }
+  }
+
+  ///适配后的屏幕高度
+  num setHeight(num height) => height * scaleHeight;
+
+  //文字适配后大小
+  num setSp(num sp) => sp * scaleText;
+}
